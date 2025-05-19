0d74877e0d81eb24cb0a1fee17d8d812307a12ad
Bilguun <145345356+Bekunee@users.noreply.github.com>
2025-05-19 13:34:51 +0900

    Delete addresses.sql

--- addresses.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS addresses (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    address_line TEXT,
-    city TEXT,
-    postal_code TEXT,
-    "default" BOOLEAN DEFAULT FALSE
-);
-
