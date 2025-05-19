55555e9ef4cb6740eb78cf376e29c706e9c5f1e3
Bilguun <145345356+Bekunee@users.noreply.github.com>
2025-05-19 13:32:53 +0900

    Delete purchases.sql

--- purchases.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS purchases (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    product_id INTEGER REFERENCES products(id),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
