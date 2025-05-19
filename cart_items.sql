fd84d8f30a1ac09c85a41985b3cff790cc0d74e3
Bilguun <145345356+Bekunee@users.noreply.github.com>
2025-05-19 13:34:30 +0900

    Delete cart_items.sql

--- cart_items.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS cart_items (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    product_id INTEGER REFERENCES products(id),
-    quantity INTEGER
-);
