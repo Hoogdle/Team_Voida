5f5cc3c649f80128153e2f0f9bbcc84b00879614
Kim TaeYeong <xodud7737@gmail.com>
2025-11-06 16:30:54 +0900

    _

--- routers/assistant_routes.py
+++ routers/assistant_routes.py
@@ -6,10 +6,11 @@ import uuid
 import bcrypt
 from utils.session_check import get_current_user
 from .product_routes import llm_model,llm_tokenizer,start_token,end_token
+import threading
 
 router = APIRouter(prefix="", tags=["Assistant"])
 
-def assistant(role ,task):
+def assistant(role ,task, stop_event):
     # LLM 모델 프롬프트 설정
     messages = [
       {"role": "system", "content": role},
@@ -27,9 +28,10 @@ def assistant(role ,task):
     )
 
     output = llm_model.generate(
-        input_ids.to("cuda"),
+        input_ids = input_ids.to("cuda"),
         eos_token_id=llm_tokenizer.eos_token_id,
         max_new_tokens=128,
+		stop_event = stop_event,
         do_sample=False,
     )
 
@@ -52,7 +54,10 @@ def assistant_category(payload: schemas.AssistantCategory, db: Session = Depends
     role = "너는 사용자의 말을 카테고리에 맞게 분류해야돼."
     task = f'"{payload.voiceInput}" 이렇게 사용자 질문이 주어졌을 때, 이 질문이 다음의 "상품검색,계정설정,카테고리,장바구니,인기상품,할인상품,배송지 변경,결제수단 변경, 도움말, 해당없음" 중 어느 곳에 속하는지 알려줘. 이때, 어느 것에 속한다고 그 이유는 출력할 필요는 없고, 그냥 속하는 부분의 명칭만 알려주면 돼."'
 
-    result = assistant(role, task)
+    stop_event = threading.Event()
+
+    result = assistant(role, task, stop_event)
+	
 
     return {"category" : result}
 
@@ -62,7 +67,8 @@ def assistant_category(payload: schemas.AssistantSearch, db: Session = Depends(g
     role = "너는 사용자의 입력에서 핵심 검색 키워드를 찾아야돼."
     task = f"{payload.voiceInput} 이렇게 사용자 질문이 주어졌을 때, 이를 검색과 관련된 문장으로 간주하고, 해당 내용에서 검색과 관련된 핵심 키워드만을 출력해줘. 왜 그렇게 생각했는지는 출력할 필요 없고 핵심 키워드만 알려주면돼. 절대 다른 문장을 추가하지 말고, 핵심 키워드만 추출해서 그것만 출력해줘. 핵심 키워드는 상품과 관련된 것으로 즉, 상품명만 출력하면돼.  예시) 햇사과를 검색해줘. 예시 답안) 햇사과"
 
-    result = assistant(role, task)
+    stop_event = threading.Event()
+    result = assistant(role, task, stop_event)
 
     return {"keyword" : result}
 
